1fbd7ab999dfda05aa415c714050508416a4d9c3
Ryusk22 <kuppo0211@docomo.ne.jp>
2019-10-30 16:21:12 +0900

    Add gem annotate

--- server/app/models/task.rb
+++ server/app/models/task.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id           :integer          not null, primary key
+#  is_completed :boolean
+#  user_id      :integer
+#  title        :string(255)      default(""), not null
+#  content      :string(255)      default(""), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Task < ApplicationRecord
 end
--- server/test/models/task_test.rb
+++ server/test/models/task_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id           :integer          not null, primary key
+#  is_completed :boolean
+#  user_id      :integer
+#  title        :string(255)      default(""), not null
+#  content      :string(255)      default(""), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
