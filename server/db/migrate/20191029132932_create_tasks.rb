d143639fd50455eb4db61f407ff1b7bae7f78342
Ryusk22 <kuppo0211@docomo.ne.jp>
2019-10-30 16:40:28 +0900

    Modify constraint condition in tasks table

--- server/db/migrate/20191029132932_create_tasks.rb
+++ server/db/migrate/20191029132932_create_tasks.rb
@@ -1,10 +1,10 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.boolean :is_completed
+      t.boolean :is_completed, null: false, default: false
       t.integer :user_id
       t.string :title, null: false, default: ""
-      t.string :content, null: false, default: ""
+      t.string :content, default: ""
       t.timestamps
     end
   end
